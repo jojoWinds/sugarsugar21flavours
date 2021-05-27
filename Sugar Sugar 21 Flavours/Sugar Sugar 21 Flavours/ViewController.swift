3757304ee454ad4822b7ac4ec35969ceb01299af
Rania Rejdal <rrejdal@uwaterloo.ca>
2021-05-27 18:44:29 -0400

    First commit

---
+++ Sugar Sugar 21 Flavours/Sugar Sugar 21 Flavours/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sugar Sugar 21 Flavours
+//
+//  Created by Rania Rejdal on 2021-05-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
